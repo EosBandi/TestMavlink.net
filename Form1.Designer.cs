ef9650f687c586d50ee086ff01bc2d7d805a68d8
Amin Ebrahimi <amin.eb.347@outlook.com>
2016-10-24 10:26:51 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MavlinkMonitorTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlConnect = new System.Windows.Forms.Panel();
+            this.lblConnect = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblYaw = new System.Windows.Forms.Label();
+            this.lblPitch = new System.Windows.Forms.Label();
+            this.lblRoll = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlConnect.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlConnect
+            // 
+            this.pnlConnect.BackColor = System.Drawing.Color.NavajoWhite;
+            this.pnlConnect.Controls.Add(this.lblConnect);
+            this.pnlConnect.Controls.Add(this.btnConnect);
+            this.pnlConnect.Location = new System.Drawing.Point(24, 12);
+            this.pnlConnect.Name = "pnlConnect";
+            this.pnlConnect.Size = new System.Drawing.Size(978, 190);
+            this.pnlConnect.TabIndex = 0;
+            // 
+            // lblConnect
+            // 
+            this.lblConnect.AutoSize = true;
+            this.lblConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblConnect.Location = new System.Drawing.Point(360, 68);
+            this.lblConnect.Name = "lblConnect";
+            this.lblConnect.Size = new System.Drawing.Size(81, 69);
+            this.lblConnect.TabIndex = 1;
+            this.lblConnect.Text = "...";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(26, 52);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(283, 85);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lblYaw);
+            this.panel2.Controls.Add(this.lblPitch);
+            this.panel2.Controls.Add(this.lblRoll);
+            this.panel2.Location = new System.Drawing.Point(24, 222);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(978, 386);
+            this.panel2.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(54, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 55);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Yaw";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(54, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 55);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Pitch";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(54, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 55);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Roll";
+            // 
+            // lblYaw
+            // 
+            this.lblYaw.AutoSize = true;
+            this.lblYaw.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYaw.Location = new System.Drawing.Point(365, 297);
+            this.lblYaw.Name = "lblYaw";
+            this.lblYaw.Size = new System.Drawing.Size(42, 40);
+            this.lblYaw.TabIndex = 0;
+            this.lblYaw.Text = "0";
+            // 
+            // lblPitch
+            // 
+            this.lblPitch.AutoSize = true;
+            this.lblPitch.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPitch.Location = new System.Drawing.Point(365, 180);
+            this.lblPitch.Name = "lblPitch";
+            this.lblPitch.Size = new System.Drawing.Size(42, 40);
+            this.lblPitch.TabIndex = 0;
+            this.lblPitch.Text = "0";
+            // 
+            // lblRoll
+            // 
+            this.lblRoll.AutoSize = true;
+            this.lblRoll.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoll.Location = new System.Drawing.Point(365, 62);
+            this.lblRoll.Name = "lblRoll";
+            this.lblRoll.Size = new System.Drawing.Size(42, 40);
+            this.lblRoll.TabIndex = 0;
+            this.lblRoll.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tomato;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(24, 625);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(978, 494);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(466, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 1176);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlConnect);
+            this.Name = "Form1";
+            this.Text = "VAIO Team";
+            this.pnlConnect.ResumeLayout(false);
+            this.pnlConnect.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlConnect;
+        private System.Windows.Forms.Label lblConnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblYaw;
+        private System.Windows.Forms.Label lblPitch;
+        private System.Windows.Forms.Label lblRoll;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Forms;
+using MavLinkNet;
+
+namespace MavlinkMonitorTest
+{
+    public partial class Form1 : Form
+    {
+        public static MavLinkSerialPortTransport mluc = new MavLinkSerialPortTransport();
+
+
+        private MavLinkSerialPortTransport mMavLink;
+        private UasAttitude mAttitudeState;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            mluc.SerialPortName = "COM22";
+            mluc.BaudRate = 57600;
+            mluc.HeartBeatUpdateRateMs = 1000;
+
+            mluc.OnPacketReceived += OnMavLinkPacketReceived;
+
+
+            mluc.Initialize();
+            mluc.BeginHeartBeatLoop();
+
+            pnlConnect.BackColor = Color.ForestGreen;
+            lblConnect.Text = "Connected";
+            btnConnect.Enabled = false;
+        }
+
+        private void OnMavLinkPacketReceived(object sender, MavLinkPacket e)
+        {
+            PrintPacket(e);
+        }
+
+        private void PrintPacket(MavLinkPacket p)
+        {
+            PrintMessage(p.Message);
+        }
+
+        private void PrintMessage(UasMessage m)
+        {
+            
+            UasMessageMetadata md = m.GetMetadata();
+            foreach (UasFieldMetadata f in md.Fields)//==================================================================
+            {
+                GetFieldValue(f.Name, m);//============================================================
+            //    //f.Name, GetFieldValue(f.Name, m), f.Description
+            }
+        }
+
+        private void GetFieldValue(string fieldName, UasMessage m)
+        {
+            if (fieldName == "Roll")
+            {
+                PropertyInfo p = m.GetType().GetProperty(fieldName);
+                Double degree = Convert.ToDouble(p.GetValue(m, null).ToString()) * (180 / Math.PI);
+                lblRoll.Invoke((MethodInvoker)(() => lblRoll.Text = degree.ToString()));
+                //lblRoll.Text= p.GetValue(m, null).ToString();
+            }
+            else if (fieldName == "Pitch")
+            {
+                PropertyInfo p = m.GetType().GetProperty(fieldName);
+                Double degree = Convert.ToDouble(p.GetValue(m, null).ToString()) * (180 / Math.PI);
+                lblPitch.Invoke((MethodInvoker)(() => lblPitch.Text = degree.ToString()));
+                //lblPitch.Text = p.GetValue(m, null).ToString();
+            }
+            else if (fieldName == "Yaw")
+            {
+                PropertyInfo p = m.GetType().GetProperty(fieldName);
+                Double degree = Convert.ToDouble(p.GetValue(m, null).ToString())*(180/Math.PI);
+                lblYaw.Invoke((MethodInvoker)(() => lblYaw.Text = degree.ToString()));
+                //lblYaw.Text = p.GetValue(m, null).ToString();
+            }
+            //if (fieldName != "Rxerrors" && fieldName != "TimeBootMs" && fieldName != "TimeUsec"&& fieldName != "Chan1Raw" && fieldName != "Fixed" && fieldName != "Chan2Raw" && fieldName!= "VtolState" && fieldName!= "Chan3Raw" && fieldName!= "Rssi" && fieldName!="Q" && fieldName!= "Chan4Raw")
+            //{
+            //    PropertyInfo p = m.GetType().GetProperty(fieldName);
+            //    var resulti = p.GetValue(m,null);
+            //    object result = p.GetValue(m, null);
+            //}
+
+
+            //if (p == null)
+            //{
+            //    WL("MISSING FIELD: {0} on {1}", fieldName, m.GetType());
+            //    return "";
+            //}
+
+
+
+            //if (result is char[]) return new String((char[])result);
+
+            //return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
